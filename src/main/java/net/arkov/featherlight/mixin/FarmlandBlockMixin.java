4caa888b58d9c97d16ba37519c8f2ba126d74e9b
Arkov <dfffhtu24794@protonmail.com>
2024-06-23 12:09:48 -0400

    Made it work on horses

--- src/main/java/net/arkov/featherlight/mixin/FarmlandBlockMixin.java
+++ src/main/java/net/arkov/featherlight/mixin/FarmlandBlockMixin.java
@@ -1,43 +1,32 @@
 package net.arkov.featherlight.mixin;
 
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FarmlandBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FarmlandBlock.class)
-public abstract class FarmlandBlockMixin {
-    @Shadow
-    public static void setToDirt(@Nullable Entity entity, BlockState state, World world, BlockPos pos) {
+public abstract class FarmlandBlockMixin extends Block {
+    public FarmlandBlockMixin(AbstractBlock.Settings settings) {
+        super(settings);
     }
 
-    @Redirect(method = "onLandedUpon",
-            at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/entity/Entity;Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"))
-    public void method(Entity entity, BlockState state, World world, BlockPos pos) {
-        if (entity instanceof PlayerEntity player) {
-//            ItemStack boots = player.getInventory().getArmorStack(0);
-//
-//            if (EnchantmentHelper.getLevel(Enchantments.FEATHER_FALLING, boots) <= 0) {
-//                setToDirt(entity, state, world, pos);
-//            }
-
-            if (EnchantmentHelper.getEquipmentLevel(Enchantments.FEATHER_FALLING, (LivingEntity) entity) <= 0) {
-                setToDirt(entity, state, world, pos);
-            }
+    @Inject(method = "onLandedUpon", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/entity/Entity;Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"), cancellable = true)
+    public void featherFallChecking(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
+        if (EnchantmentHelper.getEquipmentLevel(Enchantments.FEATHER_FALLING, (LivingEntity) entity) > 0) {
+            super.onLandedUpon(world, state, pos, entity, fallDistance);
+            ci.cancel();
         }
     }
-
 
 }
